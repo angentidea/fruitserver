b187494bd9a5568d1150d89ac61473d0f0fcb561
angentidea <angentidea@gmail.com>
2025-08-12 10:27:07 +0100

    adds cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
 
@@ -7,6 +8,7 @@ const logger = require("./logger")
 
 const { fruitsRouter } = require("./routes/fruits")
 
+app.use(cors())
 app.use(logger)
 app.use("/fruits", fruitsRouter)
 
